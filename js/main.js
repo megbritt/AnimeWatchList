ccc4f344557311afa4ba36a85f692ffcaf4146d4
Megan Martinez <mmart085@ucr.edu>
2022-01-04 15:12:24 -0800

    Added event listener for XMLHttpRequest.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ function handleSubmit() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.jikan.moe/v3/search/anime?q=' + $searchInput.value);
   xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+  });
   xhr.send();
 }
 
